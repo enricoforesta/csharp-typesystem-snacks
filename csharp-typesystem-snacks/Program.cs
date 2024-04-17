ff695b31ee0a39575f472b92e1144a4ccdba0e32
enricoforesta <enrico.foresta97@gmail.com>
2024-04-17 14:30:18 +0200

    feat: snack 1

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            /* 
+            Snack 1
+            L’utente inserisce due numeri in successione.
+            Il software stampa il maggiore.
+            */
+            Console.WriteLine($"inserisci due numeri");
+
+            int primoNumero = Convert.ToInt32(Console.ReadLine());
+            int secondoNumero = Convert.ToInt32(Console.ReadLine());
+           if(primoNumero == secondoNumero){
+                Console.WriteLine($"i numeri sono uguali, inserisci due numeri diversi");
+
+                primoNumero = Convert.ToInt32(Console.ReadLine());
+                secondoNumero = Convert.ToInt32(Console.ReadLine());
+            }
+            else if(primoNumero > secondoNumero)
+            {
+                Console.WriteLine($"il numero maggiore è {primoNumero}");
+            }
+            else
+            {
+                Console.WriteLine($"il numero maggiore è {secondoNumero}");
+
+            }
+            Console.WriteLine($"I numeri sono {primoNumero}-{secondoNumero}");
+
+
+
+
+
+
         }
     }
 }
